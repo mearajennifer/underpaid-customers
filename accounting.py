b541ef5b1f1484341636607d6a35c9cf8a269122
Jennifer Meara <jenniferlmeara@gmail.com>
2018-06-22 00:50:05 +0000

    Consolidated code into a function

--- accounting.py
+++ accounting.py
@@ -1,55 +1,27 @@
-melon_cost = 1.00
+def print_underpaid(order_file):
+    """
+    Intakes file with customer number, name, melon amount, amount paid.
+    Compares expected amount paid to actual amount paid.
+    Prints out customers who underpaid.
+    """
+    orders = open(order_file)
+    melon_cost = 1.0
 
-customer1_name = "Joe"
-customer1_melons = 5
-customer1_paid = 5.00
+    for line in orders:
+        line = line.rstrip()
+        words = line.split('|')
 
-customer2_name = "Frank"
-customer2_melons = 6
-customer2_paid = 6.00
+        cust_num, cust_name, melon_ordered, amt_paid = words
+        melon_ordered = float(melon_ordered)
+        amt_paid = float(amt_paid)
 
-customer3_name = "Sally"
-customer3_melons = 3
-customer3_paid = 3.00
+        exp_paid = melon_ordered * melon_cost
 
-customer4_name = "Sean"
-customer4_melons = 9
-customer4_paid = 9.50
+        if exp_paid != amt_paid:
+            print(cust_name, "paid {:.2f}, expected {:.2f}".format(
+                amt_paid, exp_paid))
 
-customer5_name = "David"
-customer5_melons = 4
-customer5_paid = 4.00
+    orders.close()
 
-customer6_name = "Ashley"
-customer6_melons = 3
-customer6_paid = 2.00
 
-customer1_expected = customer1_melons * melon_cost
-if customer1_expected != customer1_paid:
-    print(customer1_name, "paid {:.2f}, expected {:.2f}".format(
-        customer1_paid, customer1_expected))
-
-customer2_expected = customer2_melons * melon_cost
-if customer2_expected != customer2_paid:
-    print(customer2_name, "paid {:.2f}, expected {:.2f}".format(
-        customer2_paid, customer2_expected))
-
-customer3_expected = customer3_melons * melon_cost
-if customer3_expected != customer3_paid:
-    print(customer3_name, "paid {:.2f}, expected {:.2f}".format(
-        customer3_paid, customer3_expected))
-
-customer4_expected = customer4_melons * melon_cost
-if customer4_expected != customer4_paid:
-    print(customer4_name, "paid {:.2f}, expected {:.2f}".format(
-        customer4_paid, customer4_expected))
-
-customer5_expected = customer5_melons * melon_cost
-if customer5_expected != customer5_paid:
-    print(customer5_name, "paid {:.2f}, expected {:.2f}".format(
-        customer5_paid, customer5_expected))
-
-customer6_expected = customer6_melons * melon_cost
-if customer6_expected != customer6_paid:
-    print(customer6_name, "paid {:.2f}, expected {:.2f}".format(
-        customer6_paid, customer6_expected))
+print_underpaid("customer-orders.txt")
